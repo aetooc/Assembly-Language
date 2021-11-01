40ba08321fc2682f2aa60a225f8eabbd30e9baf7
aetooc <p200107@pwr.nu.edu.pk>
2021-11-01 23:22:13 +0500

    Task

---
+++ Files/Mul.asm
@@ -0,0 +1,15 @@
+[org 0x0100]
+
+jmp start
+result: dw 0
+
+start:
+    xor ax, ax
+    mov ax, 30
+    mov bx, 2
+    mul bx
+
+    mov [result], ax
+
+mov ax, 0x4c00
+int 0x21
---
+++ Task/Inc.asm
@@ -0,0 +1,8 @@
+[org 0x0100]
+
+xor ax, ax
+mov ax, 5
+inc ax
+
+mov ax, 0x4c00
+int 0x21
